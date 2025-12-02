8ef4a865c7ee72a187d68a8a4bf195addddf2419
Verónica Gómez Agudelo <RBN74190@educastur.es>
2025-12-02 19:55:02 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //EJERCICIO 13 PARTE 2
+        //a
+        boolean frio=false;
+        System.out.println("Variable frio: "+!frio);
+
+        //b
+        boolean bueno=true;
+        boolean bonito=true;
+        boolean barato=true;
+        boolean oportunidad=bueno&&bonito&&barato;
+        System.out.println("Oportunidad "+oportunidad);
+
+        }
+    }
