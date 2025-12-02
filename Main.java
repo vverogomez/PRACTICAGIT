6f688a0227cb9ed61df7e94fb16f786fd9d6e47b
Verónica Gómez Agudelo <RBN74190@educastur.es>
2025-12-02 19:55:28 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,42 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        //EJERCICIO 13 PARTE 2
-        //a
-        boolean frio=false;
-        System.out.println("Variable frio: "+!frio);
+        //EJEMPLO SCANNER
 
-        //b
-        boolean bueno=true;
-        boolean bonito=true;
-        boolean barato=true;
-        boolean oportunidad=bueno&&bonito&&barato;
-        System.out.println("Oportunidad "+oportunidad);
+        Scanner sc=new Scanner(System.in);
 
-        }
+
+        //LEER CADENA
+        System.out.println("Introduce nombre");
+        String nombre=sc.nextLine();
+        System.out.println("Nombre: "+nombre);
+
+        //LEER CARACTER
+        System.out.println("Introduce un caracter");
+        char letra=sc.next().charAt(0);
+        System.out.println("Letra: "+letra);
+
+        //LEER ENTERO
+        System.out.println("Introduce entero");
+        int num=sc.nextInt();
+        System.out.println("Numero: "+num);
+
+        //LEER ENTERO LONG
+        System.out.println("Introduce un entero long");
+        long num2=sc.nextLong();
+        System.out.println("Numero: "+num2);
+
+        //LEER DOUBLE
+        System.out.println("Introduce decimal");
+        double num3=sc.nextDouble();
+        System.out.println("Numero decimal: "+num3);
+
+
+
+
     }
+}
