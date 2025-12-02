c6d0fd6ce2dd0621d11a31d81d9e91b45f5a24b2
Veronica Gomez Agudelo <veronicaga29@gmail.com>
2025-12-02 20:32:29 +0100

    Hemos subido el archivo Empleado

---
+++ Empleado.java
@@ -0,0 +1,31 @@
+package ejercicio6;
+
+public class Empleado {
+
+    private int numeroEmpleado;
+    private static int numero=0;
+
+    public Empleado(){
+        numero++;
+        numeroEmpleado=numero;
+
+    }
+
+    public static int getNumeroEmpleado() {
+        return numero;
+    }
+
+    public void setNumeroEmpleado(int numeroEmpleado) {
+        this.numeroEmpleado = numeroEmpleado;
+    }
+
+    public static int getNumero() {
+        return numero;
+    }
+
+    public static void setNumero(int numero) {
+        Empleado.numero = numero;
+    }
+}
+
+
